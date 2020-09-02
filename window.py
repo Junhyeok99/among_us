f4b4bac3f44ab82707cd25e633d12b9b0690f22c
Junhyeok99 <apple2870907@gmail.com>
2020-09-02 18:51:52 +0900

    add map coloring

--- window.py
+++ window.py
@@ -26,13 +26,14 @@ def run():
     # Beginning Game Loop
     while True:
         pygame.display.update()
-        window.fill((255, 255, 255))
         p.events()
-        update(p)
-        pygame.draw.circle(window, (0, 0, 0), (p.x, p.y), 5)
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
+        update(p)
+        window.fill((255, 0, 0))
+        pygame.draw.rect(window, (255, 255, 255), (145, 95, 110, 110))
+        pygame.draw.circle(window, (0, 0, 0), (p.x, p.y), 10)
         pygame.display.flip()
         FramePerSec.tick(FPS)
